3e3ad8769aa74e9f3098a9f23cc0fe6a2dd42919
NavinPinkman <navinvenkatv@gmail.com>
2024-10-12 20:50:00 +0530

    added common zod and to export to backend and infered to frontend by publishing in npm

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,26 +1,34 @@
 import z from 'zod'
 
-const signupSchema = z.object({
+export const signupSchema = z.object({
     email : z.string().email(),
     password : z.string().min(8),
     name : z.string().optional()
 })
 
-export type inferedsignupSchema = z.infer<typeof signupSchema>
 
-const signinSchema = z.object({
+export const signinSchema = z.object({
     email : z.string().email(),
     password : z.string().min(8)
 })
 
-export type inferedsigninSchema = z.infer<typeof signinSchema>
 
 
-const blogSchema = z.object({
+export const blogSchema = z.object({
     title : z.string(),
     content : z.string(),
     published : z.boolean()
 })
 
-export type inferedblogSchema = z.infer<typeof blogSchema>
+
+export const updateBlog = z.object({
+    title : z.string(),
+    content : z.string(),
+    id : z.number()
+})
+
+export type updateblogInput = z.infer<typeof updateBlog> 
+export type blogInput = z.infer<typeof blogSchema>
+export type signinInput = z.infer<typeof signinSchema>
+export type signupInput = z.infer<typeof signupSchema>
 
